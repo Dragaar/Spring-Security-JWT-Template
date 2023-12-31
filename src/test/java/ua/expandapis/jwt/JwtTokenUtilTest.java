1a8e803da85d2d2f4f540e4d142b4a1fd7994e6e
Ros <rosivanyshyn@gmail.com>
2023-12-31 16:14:39 +0200

    Add JwtTokenUtilTest

---
+++ src/test/java/ua/expandapis/jwt/JwtTokenUtilTest.java
@@ -0,0 +1,43 @@
+package ua.expandapis.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ua.expandapis.model.entity.User;
+import ua.expandapis.model.service.user.JwtUserDetails;
+import ua.expandapis.util.JwtTokenUtil;
+
+import static ua.expandapis.TestEnvironmentConstants.TOKEN_PREFIX;
+
+@Slf4j
+@SpringBootTest
+public class JwtTokenUtilTest {
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
+
+    private String generateToken() {
+        User user = User.builder().id(1L).username("user").password("54hWgew4236wW%").build();
+        JwtUserDetails userDetails = new JwtUserDetails(user);
+
+        return jwtTokenUtil.generateToken(userDetails);
+    }
+
+    @Test
+    public void testGenerateToken() {
+        String jwtToken = generateToken();
+        log.info("Generated token -> " + jwtToken);
+        Assertions.assertNotNull(jwtToken);
+    }
+
+    @Test
+    public void testValidateToken(){
+        String token = generateToken();
+        String jwtToken = token.replace(TOKEN_PREFIX, "");
+        User user = User.builder().id(1L).username("user").password("54hWgew4236wW%").build();
+        JwtUserDetails userDetails = new JwtUserDetails(user);
+
+        Assertions.assertTrue(jwtTokenUtil.validateToken(jwtToken, userDetails));
+    }
+}
