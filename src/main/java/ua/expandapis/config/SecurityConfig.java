0c45f47922a648153d3c24ab0f49997606251db5
Ros <rosivanyshyn@gmail.com>
2024-03-19 18:22:47 +0200

    Update SecurityConfig

--- src/main/java/ua/expandapis/config/SecurityConfig.java
+++ src/main/java/ua/expandapis/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ua.expandapis.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +8,10 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,12 +20,14 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import ua.expandapis.controller.filter.JwtRequestFilter;
+import ua.expandapis.security.filter.JwtRequestFilter;
+import ua.expandapis.security.provider.JwtAuthenticationProvider;
 import ua.expandapis.util.JwtTokenUtil;
 
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 //@EnableMethodSecurity
 public class SecurityConfig {
 
@@ -32,13 +36,15 @@ public class SecurityConfig {
     private InvalidAuthenticationEntryPoint invalidAuthenticationEntryPoint;
     private JwtRequestFilter jwtRequestFilter;
 
+    private JwtAuthenticationProvider jwtAuthenticationProvider;
     @Autowired
     public SecurityConfig(JwtTokenUtil jwtTokenUtil, @Lazy UserDetailsService userDetailsService,
-                          InvalidAuthenticationEntryPoint invalidAuthenticationEntryPoint, JwtRequestFilter jwtRequestFilter) {
+                          InvalidAuthenticationEntryPoint invalidAuthenticationEntryPoint, JwtRequestFilter jwtRequestFilter, JwtAuthenticationProvider jwtAuthenticationProvider) {
         this.jwtTokenUtil = jwtTokenUtil;
         this.userDetailsService = userDetailsService;
         this.invalidAuthenticationEntryPoint = invalidAuthenticationEntryPoint;
         this.jwtRequestFilter = jwtRequestFilter;
+        this.jwtAuthenticationProvider = jwtAuthenticationProvider;
     }
 
     @Bean
@@ -56,24 +62,34 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/user/authenticate").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated())
-                .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(
+                standardAuthenticationProvider(),
+                jwtAuthenticationProvider
+        );
+    }
+
+    /* UNSUPPORTED in new Spring
+   //https://github.com/spring-projects/spring-security/issues/11926
+   @Bean
+   public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+       log.info("AuthenticationManager -> "+config.getAuthenticationManager().getClass());
+       return config.getAuthenticationManager();
+   }*/
+
+    @Bean
+    public AuthenticationProvider standardAuthenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
     }
     @Bean
     public UserDetailsService userDetailsService() {
